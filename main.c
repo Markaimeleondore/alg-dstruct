8a205b02335ff4b78d2600167cfbcfab75840811
Markaimeleondore <75540404+Markaimeleondore@users.noreply.github.com>
2021-10-27 00:28:05 +0300

    4-YakubovMark-B15
    Вариант 15:
    Реализуйте систему управления памятью, построенную на списках пустых блоков. Используйте
    двухсвязный список и стратегию “лучший подходящий”.
    Напишите функции выделения и освобождения памяти блоками произвольного размера.
    Сравните производительность вашей системы с стандартным malloc/free

--- main.c
+++ main.c
@@ -1,235 +1,382 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
 #include <assert.h>
-///////////////////////
-#define mem (ll*)malloc(sizeof(ll))
+#ifndef __MEMALLOCATOR_H__E71A34CB
+#define __MEMALLOCATOR_H__E71A34CB
+
+#define structsize sizeof(mb)
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /*
-Условие:
-Файл содержит следующую информацию (построчно): дата и средняя температура в этот день. Считать
-файл в список таким образом, чтобы он оставался отсортированным по возрастанию средней
-температуры (при совпадении средней температуры проводить сортировку следует по дате). Вывести на
-экран информацию обо всех днях, в которых средняя температура оказалась ниже нуля. Ввести с
-клавиатуры число и проверить, есть ли в списке день с такой средней температурой.
-------------------
-ТЗ:
-    1.Сделать считывание из файла в формате структуры                                                           +
-    2.Реализовать функцию ввода и печати                                                                        +
-        2.1. Ввод должен ставить элементы по порядку неубывания, если температуры равны, сортировать по дате    +
-        2.2. Печать только отрицательных элементов                                                              +
-    3. Сделать проверку на наличие элемента в списке, ответить на запрос                                        +
-    4. Сделать тесты функций
+Реализуйте систему управления памятью, построенную на списках пустых блоков. Используйте
+двухсвязный список и стратегию “лучший подходящий”.
+Напишите функции выделения и освобождения памяти блоками произвольного размера.
+Сравните производительность вашей системы с стандартным malloc/free
 */
 
-typedef struct wowMark //Инициализируем структурку LinkedList(ll).
-{
-       int  temp;
-       int  year;
-       int  month;//number of month
-       int  day;
 
-    struct wowMark *next;
-}   ll;
 
-int comparator(int temp1, int temp2, int year1, int year2, int month1, int month2, int day1, int day2)
+typedef struct mem_block
 {
-    if(temp1 < temp2)
+    int avail;
+    int size;
+    struct mem_block *next;
+    struct mem_block *prev;
+} mb;
+
+
+static int mem_allowed = 0; //Memory required size
+       int init_checker = 0;
+
+static mb* mem_beginning; //First block init
+
+
+
+int memgetblocksize()
+{
+    return structsize;
+}
+
+int memgetminimumsize()
+{
+    return structsize;
+}
+
+
+
+
+int current_free_memory()
+{
+
+    if(mem_beginning == NULL)
+        return -1;
+
+    mb* cur = mem_beginning;
+    int cur_size = 0;
+    while(cur != NULL)
     {
-        return 1;
+        if(cur->avail == 1)
+        {
+            cur_size += (cur->size + sizeof(mb));
+        }
+        cur = cur->next;
     }
-    else if(temp1 == temp2 && year1 < year2)
+    return cur_size;
+
+}
+
+
+
+
+
+// Init memory system with memory block pMemory.
+int meminit(void *pMemory, int size)
+{
+    if(pMemory == NULL || size < structsize)
+        return -1;
+
+    if(init_checker)
+        return -1;
+
+
+    mem_beginning = (mb*)pMemory;
+
+    mem_beginning->size = size - structsize ;
+    mem_beginning->avail = 1;
+    mem_beginning->next = NULL;
+    mem_beginning->prev = NULL;
+
+    mem_allowed = size;
+
+    init_checker = 1;
+
+    return size;
+}
+
+
+
+// You can implement memory leak checks here
+void memdone()
+{
+    if(mem_beginning == NULL)
+        return;
+
+    int cur_mem_size = current_free_memory();
+
+    if(cur_mem_size == -1)
+        return;
+
+    if(cur_mem_size < mem_allowed)
     {
-        return 1;
+        fprintf(stderr, "Oh no, where is my memory?! %d allowed, but only %d availiable now \n", mem_allowed, cur_mem_size);
     }
-    else if( temp1 == temp2 && year1 == year2 && month1 < month2)
+
+
+    //printf("%d", start->next->prev->size);
+}
+
+
+
+
+
+mb* seekin_for_the_best_fittin(int size)
+{
+    if(mem_beginning == NULL)
     {
-        return 1;
+        fprintf(stderr, "Fuck my life\n");
+        return NULL;
     }
-    else if(temp1 == temp2 && year1 == year2 && month1 == month2 && day1 < day2)
+
+
+    mb* cur = mem_beginning;
+    mb* cur_best = NULL;
+    int cur_best_abs = mem_allowed + 1;
+
+    while(cur)
     {
-        return 1;
+        if(cur->avail == 1)
+        {
+            if(cur->size >= size && cur_best_abs > abs(size - cur->size)) //Best-fitting detection
+            {
+                    cur_best = cur;
+                    cur_best_abs = abs(size - cur_best->size);
+            }
+        }
+        cur = cur->next;
+    }
+        //printf("Size %d", cur_best->size);
+        return cur_best;
+}
+
+
+
+
+//void* correcting_pointer_for_memloc(void* p)
+//{
+//    mb* cur = mem_beginning;
+//    void* cur_ptr = NULL;
+//    while(cur)
+//    {
+//        cur_ptr = (void*)(cur + 1);
+//        if(cur_ptr == p)
+//        {
+//            return cur_ptr;
+//        }
+//        cur = cur->next;
+//    }
+//    return NULL;
+//}
+
+
+
+
+
+// Allocate memory block of size 'size'.
+// Returns pointer to memory block is success, 0 otherwise
+void *memalloc(int size)
+{
+    mb* best_fit = seekin_for_the_best_fittin(size);
+
+
+    if(best_fit == NULL)
+    {
+        fprintf(stderr, "NEM, honey\n");
+        return 0;
+    }
+
+    void* best_fit_ptr = (void*)(best_fit + 1);
+
+    if(best_fit_ptr == NULL)
+        return 0;
+
+   // printf("%d",best_fit->size);
+
+    if(best_fit->size > structsize + size)
+    {
+        mb* trash_block = (mb*)((char*)best_fit_ptr + size);
+
+        trash_block->size = best_fit->size - structsize - size;
+        trash_block->next = best_fit->next;
+        trash_block->prev = best_fit;
+        trash_block->avail = 1;
+
+        if(best_fit->next != NULL)
+        {
+            best_fit->next->prev = trash_block;
+        }
+        best_fit->next = trash_block;
+    }
+
+
+    best_fit->size = size;
+    best_fit->avail = 0;
+
+    return best_fit_ptr;
+}
+
+
+
+int does_point_to_the_block(void* p)
+{
+    if(p == NULL)
+    {
+        fprintf(stderr,"Haha, nice try!\n");
+        return 0;
+    }
+
+
+
+    mb* cur = mem_beginning;
+
+    mb* pointed_block = (mb*)p - 1;
+
+    while(cur)
+    {
+        if(cur == pointed_block)
+        {
+            if(pointed_block == NULL)
+            {
+                fprintf(stderr,"Sir, U've missed with the pointer!\n");
+                return 0;
+            }
+            if(pointed_block->avail == 1)
+            {
+                fprintf(stderr,"This block is already availiable!\n");
+                return 0;
+            }
+
+            return 1;
+        }
+
+        cur = cur->next;
     }
     return 0;
-
-}
-
-void addElementInOrder(ll** head, int year_value, int month_value, int day_value, int temperature_value)
-{
-    ll* c = mem;
-    c -> year = year_value;
-    c -> month   = month_value;
-    c -> day  = day_value;
-    c -> temp = temperature_value;
-    ll* cur;
-
-    if((*head) == NULL)
-    {
-        c -> next = *head;
-        *head = c;
-    }
-    else
-    {
-        cur = *head;
-
-        while(cur -> next != NULL)
-        {
-            if(comparator(cur->next->temp, c->temp, cur->next->year, c->year, cur->next->month, c->month, cur->next->day, c->day))
-                break;
-
-            cur = cur -> next;
-        }
-
-        c -> next = cur -> next;
-
-        cur ->  next = c;
-    }
 }
 
 
 
-void printList(ll *head)
+
+
+// Free memory previously allocated by memalloc
+void memfree(void *p)
 {
 
-    if(head != NULL)
+    mb* pointed_block = does_point_to_the_block(p) ? (mb*)p - 1 : NULL;
+    if(!pointed_block)
+        return;
+
+    //----------merging------------//
+
+    pointed_block->avail = 1;
+    mb* new_free_block = pointed_block; //Block for extra memory and for probable merging
+    //new_free_block->avail = 1;
+
+
+    if(pointed_block->prev != NULL && pointed_block->prev->avail == 1)
     {
 
-        printList(head->next);
-        if(head->temp < 0)
-            {
-                printf("Year:%d\nNumber of month:%d\nDay:%d\nTemperature:%d\n-----\n", head -> year, head -> month, head -> day, head -> temp);
-            }
-    }
-}
-
-int is_requested_temp_in_list(ll *head, int x)
-{
-    while(head != NULL)
-    {
-        if(head->temp == x)
-        {
-            printf("Hell, yea! This one is in the list!\n-----\nYear:%d\nNumber of month:%d\nDay:%d\nTemperature:%d\n-----\n", head -> year, head -> month, head -> day, head -> temp);
-            return head->year;
-        }
+        int pointed_block_size;
+        if(pointed_block->next)
+            pointed_block_size = (char*)pointed_block->next - (char*)(pointed_block->prev + 1);
         else
-            head = head -> next;
+            pointed_block_size = (char*)(mem_beginning + 1) + mem_allowed - (char*)(pointed_block->prev + 1);
+
+        pointed_block->prev->next = pointed_block->next;
+
+        if(pointed_block->next)
+        {
+        pointed_block->next->prev = pointed_block->prev;
+        }
+
+
+        pointed_block->prev->size = pointed_block_size;
+
+        new_free_block = pointed_block->prev;
+
     }
-    printf("There is nothing like that :(\n");
-    return 0;
-}
-//Проверка на добавление
-void ASSERT1_does___addElementInOrder___add_the_element(ll *head)
-{
-    addElementInOrder(&head, 1, 1, 1, 99);
-    //ASSERT
-    assert(head->temp == 99);
-}
-//Проверка на добавление в порядке убывания
-void ASSERT2_does___addElementInOrder___add_the_element_in_non_descending_order_of_temperature_values(ll *head)
-{
-    addElementInOrder(&head, 1, 1, 1, 99);
-    addElementInOrder(&head, 1, 1, 1, 98);
-    //ASSERT
-    assert(head->temp > head->next->temp);
-}
-//Проверки функции сравнения, вынес их в ASSERT3
-// 1. По температуре
-void ASSERT3_does_comparator_really_compare_temperature_case(void)
-{
-    assert(comparator(1, 2, 1, 1, 1, 1, 1, 1) == 1);
-}
-// 2. По году
-void ASSERT3_does_comparator_really_compare_year_case(void)
-{
-    assert(comparator(1, 1, 1, 2, 1, 1, 1, 1) == 1);
-}
-// 3. По месяцу
-void ASSERT3_does_comparator_really_compare_month_case(void)
-{
-    assert(comparator(1, 1, 1, 1, 1, 2, 1, 1) == 1);
-}
-// 4. По дню
-void ASSERT3_does_comparator_really_compare_day_case(void)
-{
-    assert(comparator(1, 1, 1, 1, 1, 1, 1, 2) == 1);
-}
-
-//Проверка ответа на запрос
-void ASSERT4_does___ans___seek_for_temperature_value_correctly_in_general(ll *head)
-{
-    addElementInOrder(&head, 1, 1, 1, 99);
-    addElementInOrder(&head, 1, 1, 1, 98);
-    addElementInOrder(&head, 1, 1, 1, 97);
-    addElementInOrder(&head, 1, 1, 1, 96);
-    addElementInOrder(&head, 1, 1, 1, 95);
-    //ASSERTS
-    assert(is_requested_temp_in_list(head, 99) == 1);
-    assert(is_requested_temp_in_list(head, 1) == 0);
-    //Erase results of assertion
-    system("clear");
-}
-
-//Проверка порядка поиска ответа на запрос
-void ASSERT5_the_order_of___ans___seeking_is_descent(ll *head)
-{
-    addElementInOrder(&head, 5, 1, 1, 99);
-    addElementInOrder(&head, 4, 1, 1, 99);
-    addElementInOrder(&head, 3, 1, 1, 99);
-    //ASSERT
-    assert(is_requested_temp_in_list(head, 99) == 5);
-    //Erase results of assertion
-    system("clear");
-}
 
 
+    if(new_free_block->next != NULL && new_free_block->next->avail == 1)
+    {
+        int new_block_size;
 
+        if(new_free_block->next->next)
+            new_block_size = (char*)new_free_block->next->next - (char*)(new_free_block + 1);
+        else
+            new_block_size = (char*)(mem_beginning + 1) + mem_allowed - (char*)(new_free_block + 1);
 
-int main()
-{
-    //Logic block: FILE
-    FILE *read_data;
-    char name[] = "testCases.text";
-
-    //Logic block: инициализируем Linked List
-    ll *head = NULL;
-    //TESTS
-    ASSERT1_does___addElementInOrder___add_the_element(head);
-    ASSERT2_does___addElementInOrder___add_the_element_in_non_descending_order_of_temperature_values(head);
-
-    ASSERT3_does_comparator_really_compare_temperature_case();
-    ASSERT3_does_comparator_really_compare_year_case();
-    ASSERT3_does_comparator_really_compare_month_case();
-    ASSERT3_does_comparator_really_compare_day_case();
-
-
-    ASSERT4_does___ans___seek_for_temperature_value_correctly_in_general(head);
-    ASSERT5_the_order_of___ans___seeking_is_descent(head);
-
-    if((read_data = fopen(name, "r")))
+        if(new_free_block->next->next != NULL)
         {
-            int year, month, day, avg;
-            addElementInOrder(&head, 0, 0, 0, 0);
-            while(fscanf(read_data,"%d %d %d\n%d\n", &year, &month, &day, &avg) != EOF)
-            {
-                    addElementInOrder(&head, year, month, day, avg);
-
-
-            }
-            fclose(read_data);
-            printf("-----\n");
-            printList(head);
-
-            int request;
-            scanf("%d", &request);
-            is_requested_temp_in_list(head ,request);
-
-
-
-
+            new_free_block->next->next->prev =  new_free_block;
         }
+
+
+        new_free_block->next =  new_free_block->next->next;
+
+        new_free_block->size = new_block_size;
+    }
+
+
+    //lost parts grabbing
+    void* edge_of_this_block_ptr = (void*)((char*)(new_free_block + 1) + new_free_block->size);
+    void* edge_of_mem_ptr = (void*)((char*)mem_beginning + mem_allowed);
+
+    if(new_free_block->next)
+    {
+        if(edge_of_this_block_ptr != new_free_block->next)
+            new_free_block->size += (char*)new_free_block->next - (char*)edge_of_this_block_ptr;
+    }
     else
-        {
-            printf("Поменяйте название тестового файла :)");
-            return 0;
-        }
-        getchar();
+    {
+        if(edge_of_mem_ptr != edge_of_this_block_ptr)
+            new_free_block->size += ((char*)mem_beginning + mem_allowed - (char*)edge_of_this_block_ptr);
+    }
 }
+
+
+//Может быть поинтер в мемфри попадает мимо блока, надо как-то это отловить
+
+//Это не помогло
+
+
+
+
+// Return minimum size in bytes of the memory pool to allocate 0-bytes block
+// use case:
+// void *p   = 0;
+// void *ptr = malloc(memgetminimumsize() + 1);
+// meminit(ptr, memgetminimumsize() + 1)
+// p = memalloc(1); // Success!
+// memfree(p);
+// memdone();
+// free(ptr);
+
+
+
+
+// Returns size in bytes of additional information per allocation
+// use case:
+// void *p1 = 0, *p2 = 0;
+// int  memsize = memgetminimumsize() + memgetblocksize() + 2;
+// void *ptr = malloc(memsize);
+// meminit(ptr, memsize())
+// p1 = memalloc(1); // Success!
+// p2 = memalloc(1); // Success!
+// memfree(p2);
+// memfree(p1);
+// memdone();
+// free(ptr);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
